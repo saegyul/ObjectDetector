83231e69e85b0698de3dc08e6d46ade124f83fe5
YoungSang Yun <saegyul@gmail.com>
2019-07-01 23:32:05 -0700

    Initial Commit

---
+++ ObjectDetector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ObjectDetector
+//
+//  Created by Youngsang Yun on 7/1/19.
+//  Copyright © 2019 Youngsang Yun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
